77615da93f533436018f27c09013032a30554ce8
LeonelNieto <leonel.nieto@mabe.com.mx>
2023-01-13 16:45:33 -0600

    Leer trama hasta stop bit

--- Main.py
+++ Main.py
@@ -15,7 +15,15 @@ ERD = str(input("Ingresa el ERD a leer: "))
 ser.open()
 packet = Frame.ReadErd(ERD)
 ser.write(packet)
-print((ser.read(14)).hex())
 
-    # print(ser.read())
+while (1):
+    reading = (ser.read()).hex()
 
+    if reading != "e3":
+        print(reading)
+    else:
+        break
+
+
+#e4 c0 00 f0 01 38 e3
+#e2 e4 0e c0 a1 00 00 f0 39 01 01
