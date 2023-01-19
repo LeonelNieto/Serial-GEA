b95433c391454703d770841ba858878e6196477c
LeonelNieto <leonel.nieto@mabe.com.mx>
2023-01-19 09:27:20 -0600

    Se adapta a funcion para usarse con labview

--- config_serial.py
+++ config_serial.py
@@ -1,11 +1,9 @@
 import serial
 import serial.tools.list_ports
 
-def ConfiguracionSerial():
+def ConfiguracionSerial(board):
     ports = list(serial.tools.list_ports.comports())
-    # for port in ports:
-    #     print(port.device)
-    ser = serial.Serial(ports[0].device)
+    ser = serial.Serial(ports[board].device)
     ser.baudrate = 230400
     ser.bytesize = serial.EIGHTBITS
     ser.parity = serial.PARITY_NONE
