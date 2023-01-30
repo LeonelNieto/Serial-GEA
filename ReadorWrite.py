225a79bc5ff99b763027040ffc66291e73c9d814
LeonelNieto <leonel.nieto@mabe.com.mx>
2023-01-30 17:35:44 -0600

    Clean code

--- ReadorWrite.py
+++ ReadorWrite.py
@@ -1,17 +1,13 @@
 import Crc
 
 def ReadErd(ERD, dst):
-    bitInit = "E2"
-    src = "E4"
-    cmd = "A000"
-    bitStop = "E3"
-    longitud = int(((len(bitInit + dst + src + cmd + ERD + bitStop)) + 6) / 2)
+    longitud = int(((len("E2" + dst + "E4" + "A000" + ERD + "E3")) + 6) / 2)
     lenght = "0x{:02x}".format(longitud)
     lenght = str(lenght[2: ])   
-    FrameToCalculateCrc = dst + lenght + src + cmd + ERD
+    FrameToCalculateCrc = dst + lenght + "E4" + "A000" + ERD
     crc = Crc.crc16_ccitt(FrameToCalculateCrc)
     crc = crc[2: ]
-    frame = bitInit + FrameToCalculateCrc + crc + bitStop
+    frame = "E2" + FrameToCalculateCrc + crc + "E3"
     data = bytes.fromhex(frame)
 
     return data  
