671b2ab02f61a30884c76c0f13421c96a8152197
LeonelNieto <leonel.nieto@mabe.com.mx>
2023-02-01 08:47:53 -0600

    Nuevos Cambios

--- config_serial.py
+++ config_serial.py
@@ -10,16 +10,53 @@ def ConfiguracionSerial():                                 #Para W10 seleccionar
     ser.xonxoff = False 
     ser.rtscts = False  
     ser.dsrdtr = False  
-
-    return ser
-
-def AbrirPuerto(board):
     com_ports = list(serial.tools.list_ports.comports())
-    ser.port = com_ports[board].device
+    ser.port = com_ports[0].device
     ser.open()
- 
-escritura = ConfiguracionSerial()
-AbrirPuerto(0)
-dato = "E2C00DE4A200F03901018D6EE3"
-dataWrite = bytearray.fromhex(dato)
-escritura.write(dataWrite)
+
+import serial
+import time
+
+ser = None
+
+def connect_serial(n):
+    global ser
+    # Obtener una lista de todos los puertos COM disponibles
+    com_ports = list(serial.tools.list_ports.comports())
+
+    # Verificar si hay algún puerto disponible
+    if com_ports:
+        # Configurar la conexión serial con el primer puerto disponible
+        if not ser:
+            ser = serial.Serial()
+            ser.baudrate = 9600
+            ser.port = com_ports[0].device
+        # Abrir la conexión
+        ser.open()
+        print("Se ha conectado al puerto:", ser.port)
+        
+        # Contador de tramas
+        trama_count = 0
+        # Bucle de lectura
+        start_time = time.time()
+        while trama_count < n and time.time() - start_time < 0.1:
+            # Leer los datos que llegan al puerto serial
+            data = ser.read(ser.in_waiting)
+            # Buscar el bit de inicio "E2"
+            start = data.find(b'\xE2')
+            if start != -1:
+                # Encontró el bit de inicio, buscar el bit de paro "E3"
+                stop = data.find(b'\xE3', start)
+                if stop != -1:
+                    # Encontró la trama completa, procesar los datos
+                    trama = data[start:stop + 1]
+                    print("Trama recibida:", trama)
+                    trama_count += 1
+        # Cerrar la conexión
+        ser.close()
+    else:
+        # No hay puertos disponibles
+        print("No se ha encontrado ningún puerto COM disponible.")
+
+# Llamar a la función para conectarse y leer 10 tramas
+connect_serial(10)
