6bd1e4b8c9d9dae1c961bfed2ec4facfb5466011
LeonelNieto <leonel.nieto@mabe.com.mx>
2023-03-07 09:50:37 -0600

    Cambio de folder

---
+++ src/Crc.py
@@ -0,0 +1,56 @@
+# /***********************************************************************/
+# /*                                                                     */
+# /*  FILE          : Crc.py                                             */
+# /*  DATE          : 17/02/2023                                         */
+# /*  DESCRIPTION   : CRC Calcultaor                                     */
+# /*                                                                     */
+# /*  AUTHOR        : Leonel Nieto Lara                                  */
+# /*                                                                     */
+# /*  PROJECT       : GEA3 Tool                                          */
+# /*  IDE           : Visual Studio Code                                 */
+# /*  Python Version: 3.9.13                                             */
+# */                                                                     */
+# /*  Copyright 2012-2023 Mabe TyP                                       */
+# /*  All rights reserved                                                */
+# /*                                                                     */
+# /***********************************************************************/
+
+# ***********************************************************************************************
+#  Name:          crc16_ccitt(data_hex)    
+#  Parameters:    data_hex
+#                 Data to calculate CRC Frame
+#                 Data =  dst + length + src + cmd + ERD 
+#  Returns:       Crc16 CCITT
+#  Called by:     ReadERD( )
+#                 WriteERD( )
+#                 Boatloader( )
+#  Calls:         N/A
+#  Description:   Calculate CRC16 CCITT for complete the frome to write at the serial.  
+#               
+# ***********************************************************************************************
+def crc16_ccitt(data_hex):
+    data = bytearray.fromhex(data_hex)                                      # Convierte la trama recibida a bytearray hexadecimal
+    poly = 0x1021                                                           # poly 1021
+    crc = 0x1021                                                            # Valor inicial 1021
+    for byte in data:
+        for i in range(8):
+            bit = (byte >> (7-i) & 1) == 1
+            c15 = (crc >> 15 & 1) == 1
+            crc <<= 1
+            if c15 ^ bit:
+                crc ^= poly
+    crc &= 0xffff
+    crcStr = "0x{:02x}".format(crc)
+    longitudCrc = len(crcStr)
+    if longitudCrc == 5:
+        crcStr = crcStr[0:2] + "0" + crcStr[2: ]
+    elif longitudCrc == 4:
+        crcStr = crcStr[0:2] + "00" + crcStr[2: ]
+    
+    return crcStr
+
+
+############## Datos Para Calcular CRC lectura ###################
+
+### dst ### length ### src ###    cmd    ###   ERD    ###  
+#    C0       XX        E4       XX XX        XX XX             
---
+++ src/ReadorWrite.py
@@ -0,0 +1,105 @@
+# /***********************************************************************/
+# /*                                                                     */
+# /*  FILE          : ReadorWrite.py                                     */
+# /*  DATE          : 17/02/2023                                         */
+# /*  DESCRIPTION   : Concatenate data frame                             */
+# /*                                                                     */
+# /*  AUTHOR        : Leonel Nieto Lara                                  */
+# /*                                                                     */
+# /*  PROJECT       : GEA3 Tool                                          */
+# /*  IDE           : Visual Studio Code                                 */
+# /*  Python Version: 3.9.13                                             */
+# */                                                                     */
+# /*  Copyright 2012-2023 Mabe TyP                                       */
+# /*  All rights reserved                                                */
+# /*                                                                     */
+# /***********************************************************************/
+
+import Crc
+
+# /************************************************************************
+#  Name:          ReadErd( )    
+#  Parameters:    ERD, Destination
+#  Returns:       Frame to write to the serial
+#  Called by:     ReadButton( ) from (Main.py)
+#  Calls:         crc16_ccitt( ) in (Crc.py)
+#  Description:   Concatenate the frame to write for serial with GEA, usign
+#                 BitInit, Destination, lenght, source, Command, CRC
+#                 and bit Stop
+#               
+# ************************************************************************/
+def ReadErd(ERD, dst):
+    bitInit = "E2"                                                                                      # Bit de Inicio
+    src = "E4"                                                                                          # Source
+    cmd = "A000"                                                                                        # Comando de  request para lectura
+    bitStop = "E3"                                                                                      # Bit de Stop
+    longitud = int(((len(bitInit + dst + src + cmd + ERD + bitStop)) + 6) / 2)                          # Cálculo de la longitud de la trama
+    lenght = "{:02x}".format(longitud)                                                                  # Conversión a hexadecimal de dos digitos
+    FrameToCalculateCrc = dst + lenght + src + cmd + ERD                                                # Concatena trama para calculo de CRC
+    crc = Crc.crc16_ccitt(FrameToCalculateCrc)                                                          # Calcula el CRC
+    crc = crc[2: ]                                                                                      # Elimina "0x" del CRC
+    frame = bitInit + FrameToCalculateCrc + crc + bitStop                                               # Concatena la trama de datos completa en hexadecimal
+    data = bytes.fromhex(frame)                                                                         # Convierte los datos a bytes
+    return data                                                                                         # Retorna la trama a escribir en el serial
+
+# /************************************************************************
+#  Name:          WriteErd( )    
+#  Parameters:    ERD, Dato, Destination
+#  Returns:       Frame to write to the serial
+#  Called by:     WriteButton( ) from (Main.py)
+#  Calls:         crc16_ccitt( ) in (Crc.py)
+#  Description:   Concatenate the frame to write for serial with GEA, usign
+#                 BitInit, Destination, Lenght, Source, Command, ERD,  
+#                 Dato Lenght, Dato, CRC and bit Stop
+#               
+# ************************************************************************/
+def WriteErd(ERD, dato, dst):
+    bitInit = "E2"                                                                                      # Bit de Inicio
+    src = "E4"                                                                                          # Source
+    cmd = "A200"                                                                                        # Comando de request para escritura
+    count = int((len(dato)) / 2)                                                                        # Calculo de la longitud del dato a escribir
+    count = "0x{:02x}".format(count)                                                                    # Conversión a hexadecimal de dos digitos
+    count = count[2: ]                                                                                  # Eliminar "0x" del valor hexadeciaml
+    ESC = "E0"                                                                                          # Bit de ESC
+    bitStop = "E3"                                                                                      # Bit de paro
+    longitud = int(((len(bitInit + dst + src + cmd + ERD + count + dato + bitStop)) + 6) / 2)           # Calculo de la longitud de la trama
+    lenght = "0x{:02x}".format(longitud)                                                                # Conversion de la longitud a hexadecimal de dos digitos
+    lenght = str(lenght[2: ])                                                                           # Eliminacion de "0x" del valor hexadecimlal
+    FrameToCalculateCrc = dst + lenght + src + cmd + ERD + count + dato                                 # Concatenacion de la trama para calcular el CRC
+    crc = Crc.crc16_ccitt(FrameToCalculateCrc)                                                          # Modulo para calcular CRC
+    crc = crc[2: ]                                                                                      # Se elimina el "0x" del CRC
+    if ERD != "0032":                                                                                   # Si el ERD es diferente del de reset (0032)
+        frame = bitInit + FrameToCalculateCrc + crc + bitStop                                           # Concatena la trama para escribir normalmente
+    else:                                                                                               # Si el ERD es 0032
+        frame = bitInit + FrameToCalculateCrc + ESC + crc + bitStop                                     # Se agrega el bit de ESC antes del CRC
+    dataWrite = bytearray.fromhex(frame)                                                                # Conversion de la trama a bytearray
+    return dataWrite                                                                                    # Retorna la trama a escribir en serial para escritura    
+
+# /************************************************************************
+#  Name:          Boatloader( )    
+#  Parameters:    Destination, command, message
+#  Returns:       Frame to write to the serial
+#  Called by:     ReadButton( ) from (Main.py)
+#  Calls:         crc16_ccitt( ) in (Crc.py)
+#  Description:   Concatenate the frame to write for serial with GEA, usign
+#                 BitInit, dst, lenght, src, cmd, message, CRC and bit Stop 
+#               
+# ************************************************************************/
+def Boatloader(dst, cmd, msg):
+    bitInit = "E2"                                                                                      # Bit de inicio
+    src = "E4"                                                                                          # Source
+    bitStop = "E3"                                                                                      # Bit de paro
+    longitud = int(((len(bitInit + dst + src + cmd + msg + bitStop)) + 6) / 2)                          # Calculo del tamaño de la trama sin contar bytes especiales
+    lenght = "0x{:02x}".format(longitud)                                                                # Conversión a hexadecimal de dos digitos
+    lenght = str(lenght[2: ])                                                                           # Elimina "0x" de la longitud
+    FrameToCalculateCrc = dst + lenght + src + cmd + msg                                                # Concatena la trama para el calculo de CRC
+    crc = Crc.crc16_ccitt(FrameToCalculateCrc)                                                          # Modulo para calcular CRC
+    crc = crc[2: ]                                                                                      # Elimina "0x" del CRC
+    frame = bitInit + FrameToCalculateCrc + crc + bitStop                                               # Concatena la trama para el envío del mensaje serial
+    data = bytes.fromhex(frame)                                                                         # Convierte a bytes la trama a enviar por serial
+    return data                                                                                         # Retorna la trama a escribir en el serial
+
+################################### TRAMA DE DATOS LECTURA ###############################################
+
+### Bit Inicio ### dst ### length ### src ###    cmd    ###   ERD    ###   CRC    ### Bit Stop ###
+#       E2          C0       XX        E4       XX XX        XX XX        XX XX          E3    ###
