224e001a39fb9ee2978cbcc51d91879c74709c77
LeonelNL <leonelnieto305@gmail.com>
2023-06-29 17:42:30 -0600

    .

--- src/Test/T345.py
+++ src/Test/T345.py
@@ -12,8 +12,8 @@ file_name_Test = "T1_Results"
 file_Test = os.path.join(Actual_Path, file_name_Test + ".csv")
 
 HEADERS = ["Date", "Time", "Action", "ERD", "Expected Data", "Data", "Data to Write", "Result", "Comments"]
-
 Write_Data_CSV(file_Test, HEADERS)
+
 Read("C0", Erd_Reset, "00", file_Test)
 Read("C0", Erd_MbKeyStatusWord, "000000", file_Test)
 Read("C0", Erd_ResetCount, "0000C9", file_Test)
